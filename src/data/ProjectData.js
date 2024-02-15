0a4fe73817fc2dd282fe69128ca56cf2b47624e8
Vijay Jangal <jangalvijay36@gmail.com>
2024-02-15 17:53:13 +0530

    Intial changes

---
+++ src/data/ProjectData.js
@@ -0,0 +1,104 @@
+export const ProjectList = [
+  {
+    img: "/project1.png",
+    title: "GameSpace",
+    description:
+      "Gamespace is a website consists of five games such as chess, tic-tac-toe, sudoku,minesweeper, 2048 in this you have different modes like offline,online and vs computer.",
+    tech_stack: ["HTML", "CSS", "JavaScript", "NodeJS", "Mongodb"],
+    github_url: "https://github.com/Vsjangal/Gamespace",
+    demo_url: "",
+  },
+  {
+    img: "/project2.png",
+    title: "PES using NN and GPR",
+    description:
+      "In this project i have used used python and numerous libraries like keras, tenserflow to create neural network and numpy, pandas etc. for organizing and visualizing data.",
+    tech_stack: ["Python", "Keras and Tensorflow", "Numpy"],
+    github_url:
+      "https://github.com/Vsjangal/B.tech-Project--Predicting-PES-Using-ANN-and-GPR",
+    demo_url: "",
+  },
+  {
+    img: "/project3.png",
+    title: "Spotify Clone",
+    description:
+      "In this project i have used the spotify api for developers to get the songs and my account from spotify and add functionalities accordingly.",
+    tech_stack: ["ReactJs","React-Context-Api","Spotify-Api"],
+    github_url: "https://github.com/Vsjangal/SpotifyCloneRepo",
+    demo_url: "",
+  },
+  {
+    img: "/project4.png",
+    title: "Social media website",
+    description:
+      "This project is done while i was learning it is a simple website on which you can like,comment and interact with someone's post.",
+    tech_stack: ["HTML", "CSS", "Javascript","NodeJs"],
+    github_url: "https://github.com/Vsjangal/social_media",
+    demo_url: "",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
