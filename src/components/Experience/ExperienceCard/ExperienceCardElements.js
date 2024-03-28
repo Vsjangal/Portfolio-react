95a2ffc9443d9bb8131f427858eec8f616550914
Vijay-elastic <vijay@elasticdevs.io>
2024-03-29 01:42:29 +0530

    add experience

--- src/components/Experience/ExperienceCard/ExperienceCardElements.js
+++ src/components/Experience/ExperienceCard/ExperienceCardElements.js
@@ -18,10 +18,10 @@ export const Card = styled.div`
 `;
 
 export const CardLeft = styled.div`
-  margin-top: 2rem;
   height: 100%;
   display: flex;
   align-items: center;
+  justify-content: center;
   flex-direction: column;
 `;
 
--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -1,21 +1,32 @@
 export const ExperienceList = [
-  {
-    title: "Analyst Intern",
-    company: "Big-Bites",
-    date: "15 May 2022 - 15 July 2022",
-    details: [
-      "Worked on identifying optimal set of locations for opening 30 new cloud kitchens.",
-      "Used genetic algorithm along with greedy algorithms to determine optimal locations based on order history.",
-      "Solution laid the basis for the company's in-house food delivery app.",
-    ],
-  },
-  {
-    title: "React-native intern",
-    company: "Stealth Startup",
-    date: "15 Nov 2023 - 15 Jan 2024",
-    details: [
-      "Enhanced and implemented multiple UI functionalities of gaming application.",
-      "Conducted comprehensive Usability Testing of the UI using TestFlight, leading to the identification and resolution of numerous application bugs.",
-    ],
-  },
+    {
+        title: "Software Engineer",
+        company: "Elastic Devs",
+        date: "Jan 2024 - present",
+        details: [
+            "Refining backend performance for a real estate website by leveraging Django's ORM for efficient data retrieval, replacing conventional raw SQL queries.",
+            "This employs a robust tech stack, featuring Docker, Django, Celery, and RabbitMQ for the backend, complemented by ReactJS for the frontend.",
+            "Developed frontend for decentralized web app, integrating UI design with core functionality. ",
+            "Implemented Metamask integration for real-time data access and dynamic content presentation.",
+        ],
+    },
+    {
+        title: "Analyst Intern",
+        company: "Big-Bites",
+        date: "15 May 2022 - 15 July 2022",
+        details: [
+            "Worked on identifying optimal set of locations for opening 30 new cloud kitchens.",
+            "Used genetic algorithm along with greedy algorithms to determine optimal locations based on order history.",
+            "Solution laid the basis for the company's in-house food delivery app.",
+        ],
+    },
+    {
+        title: "React-native intern",
+        company: "Stealth Startup",
+        date: "15 Nov 2023 - 15 Jan 2024",
+        details: [
+            "Enhanced and implemented multiple UI functionalities of gaming application.",
+            "Conducted comprehensive Usability Testing of the UI using TestFlight, leading to the identification and resolution of numerous application bugs.",
+        ],
+    },
 ];
