546760590ff92a63b7c62da4bc0823eeb7a6c8fb
Vijay Jangal <jangalvijay36@gmail.com>
2024-06-06 04:13:50 +0530

    added new image and add new content

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -1,32 +1,31 @@
 export const ExperienceList = [
-    {
-        title: "Software Engineer",
-        company: "Elastic Devs",
-        date: "Jan 2024 - present",
-        details: [
-            "Refining backend performance for a real estate website by leveraging Django's ORM for efficient data retrieval, replacing conventional raw SQL queries.",
-            "This employs a robust tech stack, featuring Docker, Django, Celery, and RabbitMQ for the backend, complemented by ReactJS for the frontend.",
-            "Developed frontend for decentralized web app, integrating UI design with core functionality. ",
-            "Implemented Metamask integration for real-time data access and dynamic content presentation.",
-        ],
-    },
-    {
-        title: "Analyst Intern",
-        company: "Big-Bites",
-        date: "15 May 2022 - 15 July 2022",
-        details: [
-            "Worked on identifying optimal set of locations for opening 30 new cloud kitchens.",
-            "Used genetic algorithm along with greedy algorithms to determine optimal locations based on order history.",
-            "Solution laid the basis for the company's in-house food delivery app.",
-        ],
-    },
-    {
-        title: "React-native intern",
-        company: "Stealth Startup",
-        date: "15 Nov 2023 - 15 Jan 2024",
-        details: [
-            "Enhanced and implemented multiple UI functionalities of gaming application.",
-            "Conducted comprehensive Usability Testing of the UI using TestFlight, leading to the identification and resolution of numerous application bugs.",
-        ],
-    },
+  {
+    title: "Software Engineer",
+    company: "Elastic Devs",
+    date: "Jan 2024 - present",
+    details: [
+      "Currently leading the migration of Pro Kick Sports (an online sports store) from Shopify to a custom solution, focusing on optimizing costs and enhancing customization to make operations smoother and promote business growth.",
+      "Led frontend team for GPU.net (a blockchain based GPU renting platform), playing a key role in the project’s successful launch, which secured $5M in funding upon its release. Managed UI implementation, developing marketplace UI, landing page, and wallet integration.",
+      "Led the development of Glibzter (a personality development platform) admin panel and homepage, improving user ex- perience. Implemented user-friendly admin features and engaging UI components to enhance functionality and usability.",
+    ],
+  },
+  {
+    title: "Analyst Intern",
+    company: "Big-Bites",
+    date: "15 May 2022 - 15 July 2022",
+    details: [
+      "Worked on identifying optimal set of locations for opening 30 new cloud kitchens.",
+      "Used genetic algorithm along with greedy algorithms to determine optimal locations based on order history.",
+      "Solution laid the basis for the company's in-house food delivery app.",
+    ],
+  },
+  {
+    title: "React-native intern",
+    company: "Stealth Startup",
+    date: "15 Nov 2023 - 15 Jan 2024",
+    details: [
+      "Enhanced and implemented multiple UI functionalities of gaming application.",
+      "Conducted comprehensive Usability Testing of the UI using TestFlight, leading to the identification and resolution of numerous application bugs.",
+    ],
+  },
 ];
